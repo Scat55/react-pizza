1300e95a8c5cdc70a1c4b4ca1872ee2ec84249d0
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2024-01-09 21:26:03 +0300

    feat: added category rendering and class change on click

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,13 +1,44 @@
+import React from 'react';
+
+// Создаем интерфейс описывающий категории пицц
+interface Categories {
+	id: number;
+	name: string;
+}
+
 function Categories() {
+	// Создаем массив объектов с категориями пицц
+	const categories: Categories[] = [
+		{ id: 0, name: 'Все' },
+		{ id: 1, name: 'Мясные' },
+		{ id: 2, name: 'Вегетарианская' },
+		{ id: 3, name: 'Гриль' },
+		{ id: 4, name: 'Острые' },
+		{ id: 5, name: 'Закрытые' },
+	];
+	// Создаем state для хранения состояния активной категории
+	const [activeCategory, setActiveCategory] = React.useState(0);
+
+	// Функция приравнивает число к id категории
+	const changeCategoryAfterClick = (categoryId: number) => {
+		setActiveCategory(categoryId);
+	};
 	return (
 		<div className='categories'>
 			<ul>
-				<li className='active'>Все</li>
-				<li>Мясные</li>
-				<li>Вегетарианская</li>
-				<li>Гриль</li>
-				<li>Острые</li>
-				<li>Закрытые</li>
+				{/* Делаем перебор по массиву */}
+				{categories.map(category => (
+					// Создаем тег li с классом "active" если состояние(activeCategory равна id конкретного элемента )
+					<li
+						className={activeCategory === category.id ? 'active' : ''}
+						// По клику меняем id
+						onClick={() => changeCategoryAfterClick(category.id)}
+						key={category.id}
+					>
+						{/* Выводим название пиццы */}
+						{category.name}
+					</li>
+				))}
 			</ul>
 		</div>
 	);
