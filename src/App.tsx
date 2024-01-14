1121f0dde8ca3be5012ce3a8158b50d89786d2a4
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2024-01-14 16:53:26 +0300

    feat: added Skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,17 @@ import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
 import axios from 'axios';
+import Skeleton from './components/PizzaBlock/Skeleton';
 
 function App() {
 	const [items, setItems] = React.useState([]);
+	const [isLoading, setIsLoading] = React.useState(true);
 
 	React.useEffect(() => {
 		const response = async () => {
 			await axios.get('https://19c886375be11f9f.mokky.dev/pizzas').then(res => {
 				setItems(res.data);
+				setIsLoading(false);
 			});
 		};
 		response();
@@ -29,10 +32,9 @@ function App() {
 					</div>
 					<h2 className='content__title'>Все пиццы</h2>
 					<div className='content__items'>
-						{/*  /> */}
-						{items.map(pizza => (
-							<PizzaBlock {...pizza} key={pizza.id} />
-						))}
+						{isLoading
+							? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
+							: items.map(obj => <PizzaBlock {...obj} key={obj.id} />)}
 					</div>
 				</div>
 			</div>
