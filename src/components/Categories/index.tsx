e52d6928aa71d4a1042eeed35b3ad18ec7fdacc9
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2024-01-15 23:12:11 +0300

    feat: added sort by category

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -1,46 +1,44 @@
-import React from 'react';
-
 // Создаем интерфейс описывающий категории пицц
 interface Categories {
-	id: number;
-	name: string;
+  id: number;
+  name: string;
 }
 
-function Categories() {
-	// Создаем массив объектов с категориями пицц
-	const categories: Categories[] = [
-		{ id: 0, name: 'Все' },
-		{ id: 1, name: 'Мясные' },
-		{ id: 2, name: 'Вегетарианская' },
-		{ id: 3, name: 'Гриль' },
-		{ id: 4, name: 'Острые' },
-		{ id: 5, name: 'Закрытые' },
-	];
-	// Создаем state для хранения состояния активной категории
-	const [activeCategory, setActiveCategory] = React.useState(0);
-
-	// Функция приравнивает число к id категории
-	const changeCategoryAfterClick = (categoryId: number) => {
-		setActiveCategory(categoryId);
-	};
-	return (
-		<div className='categories'>
-			<ul>
-				{/* Делаем перебор по массиву */}
-				{categories.map(category => (
-					// Создаем тег li с классом "active" если состояние(activeCategory равна id конкретного элемента )
-					<li
-						className={activeCategory === category.id ? 'active' : ''}
-						// По клику меняем id
-						onClick={() => changeCategoryAfterClick(category.id)}
-						key={category.id}
-					>
-						{/* Выводим название пиццы */}
-						{category.name}
-					</li>
-				))}
-			</ul>
-		</div>
-	);
+// Создаем интрефейс Props
+interface Props {
+  value: number;
+  onClickCategory: (id: number) => void;
 }
+
+function Categories({ value, onClickCategory }: Props) {
+  // Создаем массив объектов с категориями пицц
+  const categories: Categories[] = [
+    { id: 0, name: 'Все' },
+    { id: 1, name: 'Мясные' },
+    { id: 2, name: 'Вегетарианская' },
+    { id: 3, name: 'Гриль' },
+    { id: 4, name: 'Острые' },
+    { id: 5, name: 'Закрытые' },
+  ];
+
+  return (
+    <div className="categories">
+      <ul>
+        {/* Делаем перебор по массиву */}
+        {categories.map((category) => (
+          // Создаем тег li с классом "active" если состояние(activeCategory равна id конкретного элемента )
+          <li
+            className={value === category.id ? 'active' : ''}
+            // По клику меняем id
+            onClick={() => onClickCategory(category.id)}
+            key={category.id}>
+            {/* Выводим название пиццы */}
+            {category.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
 export default Categories;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,11 +8,19 @@ import Skeleton from '../components/PizzaBlock/Skeleton';
 function Home() {
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
+  // State Categories
+  const [categoryId, setCategoryId] = React.useState(0);
+  // State Sort
+  const [sortType, setSortType] = React.useState(0);
 
+  // Делаем запрос на сервер
   React.useEffect(() => {
+    setIsLoading(true);
     const response = async () => {
       await axios
-        .get('https://65a56f1352f07a8b4a3f1aa3.mockapi.io/pizzas')
+        .get(
+          `https://65a56f1352f07a8b4a3f1aa3.mockapi.io/pizzas?category=${categoryId}`,
+        )
         .then((res) => {
           setItems(res.data);
           setIsLoading(false);
@@ -20,11 +28,15 @@ function Home() {
       window.scrollTo(0, 0);
     };
     response();
-  }, []);
+  }, [categoryId]);
+
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
+        <Categories
+          value={categoryId}
+          onClickCategory={(i) => setCategoryId(i)}
+        />
         <Sort />
       </div>
       <h2 className="content__title">Все пиццы</h2>
