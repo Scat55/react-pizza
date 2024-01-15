4ec1457949d17d32d21c728b376cd83a3dfa0fa9
Dmitry Bystrov <dmit.bystroff2016@yandex.ru>
2024-01-15 22:18:33 +0300

    refactor: added prettier

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,33 +4,39 @@ import Sort from '../components/Sort';
 import PizzaBlock from '../components/PizzaBlock';
 import axios from 'axios';
 import Skeleton from '../components/PizzaBlock/Skeleton';
-function Home() {
-	const [items, setItems] = React.useState([]);
-	const [isLoading, setIsLoading] = React.useState(true);
 
-	React.useEffect(() => {
-		const response = async () => {
-			await axios.get('https://65a56f1352f07a8b4a3f1aa3.mockapi.io/pizzas').then(res => {
-				setItems(res.data);
-				setIsLoading(false);
-			});
-			window.scrollTo(0, 0);
-		};
-		response();
-	}, []);
-	return (
-		<div className='container'>
-			<div className='content__top'>
-				<Categories />
-				<Sort />
-			</div>
-			<h2 className='content__title'>Все пиццы</h2>
-			<div className='content__items'>
-				{isLoading
-					? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-					: items.map(obj => <PizzaBlock {...obj} key={obj.id} />)}
-			</div>
-		</div>
-	);
+function Home() {
+  const [items, setItems] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    const response = async () => {
+      await axios
+        .get('https://65a56f1352f07a8b4a3f1aa3.mockapi.io/pizzas')
+        .then((res) => {
+          setItems(res.data);
+          setIsLoading(false);
+        });
+      window.scrollTo(0, 0);
+    };
+    response();
+  }, []);
+  return (
+    <div className="container">
+      <div className="content__top">
+        <Categories />
+        <Sort />
+      </div>
+      <h2 className="content__title">Все пиццы</h2>
+      <div className="content__items">
+        {isLoading
+          ? [...new Array(6)].map((_, index) => (
+              <Skeleton key={index} />
+            ))
+          : items.map((obj) => <PizzaBlock {...obj} key={obj.id} />)}
+      </div>
+    </div>
+  );
 }
+
 export default Home;
